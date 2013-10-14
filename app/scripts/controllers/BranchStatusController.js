1f9e68a4b1168edb8cc9a6caa7a71779f1981370
Nikolai Muhhin <nikolai.muhhin@gmail.com>
2013-10-14 23:02:47 +0300

    Remove Github branches

--- app/scripts/controllers/BranchStatusController.js
+++ app/scripts/controllers/BranchStatusController.js
@@ -2,25 +2,21 @@
 
 
 app.controller('BranchStatusController', [
-    '$scope', 'schedule', 'jenkins', 'github',
+    '$scope', 'schedule', 'jenkins', 
     'createBuildCard', 'createGoogle3Card', 'createShaCountCard',
     function BranchStatusController(
-        $scope, schedule, jenkins, github,
+        $scope, schedule, jenkins, 
         createBuildCard, createGoogle3Card, createShaCountCard) {
 
   var masterBuildCard = createBuildCard();
   var stableBuildCard = createBuildCard();
-  var masterGoogle3Card = createGoogle3Card();
-  var stableGoogle3Card = createGoogle3Card();
-  var masterReleaseCard = createShaCountCard();
-  var stableReleaseCard = createShaCountCard();
 
   $scope.branches = [{
     title: 'master',
-    cards: [masterBuildCard, masterGoogle3Card, masterReleaseCard]
+    cards: [masterBuildCard]
   }, {
     title: 'stable/1.0',
-    cards: [stableBuildCard, stableGoogle3Card, stableReleaseCard]
+    cards: [stableBuildCard]
   }];
 
   schedule.onceAMinute(function() {
@@ -33,20 +29,5 @@ app.controller('BranchStatusController', [
       stableBuildCard.update(buildStatus.happy, buildStatus.since, buildStatus.author);
       $scope.$emit('dash:buildUpdate', 'stable/1.0', buildStatus);
     });
-
-    github.getSHAsSince('master', 'g3_v1_x').then(function(count) {
-      masterGoogle3Card.update(count);
-    });
-    github.getSHAsSince('v1.0.x', 'g3_v1_0').then(function(count) {
-      stableGoogle3Card.update(count);
-    });
-
-    github.getSHAsSinceRelease('master').then(function(count) {
-      masterReleaseCard.update(count);
-    });
-
-    github.getSHAsSinceRelease('v1.0.x').then(function(count) {
-      stableReleaseCard.update(count);
-    })
   });
 }]);
